7e2f25329ba78cc20f33a5cc742fa102817b2016
kinloli21 <48921276+kinloli21@users.noreply.github.com>
2024-01-29 11:06:19 +0200

    Codes for the game
    All scripts are here

---
+++ GameOver.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+    public void Restart()
+    {
+        SceneManager.LoadSceneAsync(1);
+    }
+    public void MainMenu()
+    {
+        SceneManager.LoadSceneAsync(0);
+    }
+
+}
